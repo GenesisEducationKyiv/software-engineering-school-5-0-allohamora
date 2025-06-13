4ae27613628f9ba9ca257150d87470cc75b849b8
allohamora <herman.hrand@gmail.com>
2025-06-13 13:50:19 +0300

    refactor: add implements ConfigService to ZnvConfigService

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -29,7 +29,7 @@ export type ConfigService = {
   get<T extends keyof Config>(key: T): Config[T];
 };
 
-export class ZnvConfigService {
+export class ZnvConfigService implements ConfigService {
   protected config: Config;
 
   constructor() {
