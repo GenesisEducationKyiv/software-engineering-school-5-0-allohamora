00c2d600f22965317e3586ea91c6367bddfcd475
allohamora <herman.hrand@gmail.com>
2025-07-06 17:54:48 +0300

    test: add exceptions to toThrow calls in email provider tests

--- __tests__/integration/email.provider.spec.ts
+++ __tests__/integration/email.provider.spec.ts
@@ -34,17 +34,8 @@ describe('ResendEmailProvider (integration)', () => {
           to,
         }),
       ),
-    unauthorized: () =>
-      emailApi.mock(() => HttpResponse.json({ error: { message: 'Invalid API key' } }, { status: 401 })),
-    badRequest: () =>
-      emailApi.mock(() =>
-        HttpResponse.json(
-          {
-            error: { message: 'Invalid request' },
-          },
-          { status: 400 },
-        ),
-      ),
+    unauthorized: () => emailApi.mock(() => HttpResponse.json({ message: 'Invalid API key' }, { status: 401 })),
+    badRequest: () => emailApi.mock(() => HttpResponse.json({ message: 'Invalid request' }, { status: 400 })),
   };
 
   beforeAll(() => {
@@ -118,13 +109,11 @@ describe('ResendEmailProvider (integration)', () => {
             text: 'This is a test email',
           },
         }),
-      ).rejects.toThrow(Exception);
+      ).rejects.toThrow(Exception.InternalServerError('Invalid request'));
 
       expect(errorSpy).toHaveBeenCalledWith({
         err: {
-          error: {
-            message: 'Invalid request',
-          },
+          message: 'Invalid request',
         },
       });
     });
@@ -141,13 +130,11 @@ describe('ResendEmailProvider (integration)', () => {
             text: 'This is a test email',
           },
         }),
-      ).rejects.toThrow(Exception);
+      ).rejects.toThrow(Exception.InternalServerError('Invalid API key'));
 
       expect(errorSpy).toHaveBeenCalledWith({
         err: {
-          error: {
-            message: 'Invalid API key',
-          },
+          message: 'Invalid API key',
         },
       });
     });
