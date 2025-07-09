4b1e6bffd0fb808b0b4ae3fc777718ebf1ce7463
allohamora <herman.hrand@gmail.com>
2025-07-09 17:56:33 +0300

    refactor(email): fix weatherService typo

--- apps/email/src/controllers/email.controller.ts
+++ apps/email/src/controllers/email.controller.ts
@@ -2,16 +2,16 @@ import { Server } from 'nice-grpc';
 import { EmailService } from 'src/services/email.service.js';
 import { EmailServiceDefinition } from '@weather-subscription/proto/email';
 
-export const makeEmailRoutes = (server: Server, weatherService: EmailService) => {
+export const makeEmailRoutes = (server: Server, emailService: EmailService) => {
   server.add(EmailServiceDefinition, {
     sendSubscribeEmail: async (options) => {
-      await weatherService.sendSubscribeEmail(options);
+      await emailService.sendSubscribeEmail(options);
 
       return {};
     },
 
     sendWeatherUpdateEmail: async (options) => {
-      await weatherService.sendWeatherUpdateEmail(options);
+      await emailService.sendWeatherUpdateEmail(options);
 
       return {};
     },
