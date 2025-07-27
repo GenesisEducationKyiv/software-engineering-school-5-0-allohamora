58fc31fe5bf6f76f47a13eb02d7c78d4b3b234e8
allohamora <herman.hrand@gmail.com>
2025-07-27 09:08:26 +0300

    refactor(queue): remove unused toSnakeCase

--- libs/queue/src/utils/case.utils.ts
+++
@@ -1,6 +0,0 @@
-export const toSnakeCase = (string: string): string => {
-  return string
-    .replace(/([a-z])([A-Z])/g, '$1_$2')
-    .replace(/([A-Z])([A-Z][a-z])/g, '$1_$2')
-    .toLowerCase();
-};
